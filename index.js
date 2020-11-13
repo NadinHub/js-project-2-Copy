d2dae79bea7024798aeab421f2bd2f9be05d92b2
Nadezhda <thenadejdak@gmail.com>
2020-11-13 20:34:01 +0200

    m3-F added price and improvment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-const btnSearch = document.getElementById('btnSearchID')
-const inputSearch = document.getElementById('inputID')
+const btnSearch = document.getElementById('btnSearchID');
+const inputSearch = document.getElementById('inputID');
+const emptybox = document.getElementById('emptybox');
+resultList = document.getElementById('result-list');
 
 //{symbol: "ASRVP", name: "Ameriserv Financial Capital Trust I PFD A GTD 8.45", currency: "USD", stockExchange: "NasdaqGM", exchangeShortName: "NASDAQ"}
 
@@ -12,40 +14,47 @@ function removeSpiner(loaderParameter) {
     loaderParameter.classList.remove('loader');
     loaderParameter.classList.add('loader-opacity-0');
 }
-resultList = document.getElementById('result-list');
 let stockName;
-;
-let arrayOfImages = [];
 
-function showTheList(dataParam) { // show all results (from server) on the page
+
+function showTheList(dataParam, arrayOfImages, arrayOfChanges, arrayOfPrice) { 
+    // show all results (from server) on the page
     for (let i = 0; i < dataParam.length; i++) {
-
-        newItem = document.createElement('li');
+        let newItem = document.createElement('li');
         newItem.classList.add('list-group-item', 'pb-4', 'pl-0');
         resultList.appendChild(newItem);
-        // let myDate = new Date(dataParam[i].createdDate); //transform dates
-        newItem.innerHTML = `<img class="img-icon" src="${arrayOfImages[i]}"></img><a href="/company.html?symbol=${dataParam[i].symbol}"><b>${dataParam[i].name}</b> (${dataParam[i].symbol})</a>  <small class="text-secondary">- ${dataParam[i].exchangeShortName}</small> <br> <small class="text-secondary">Currency: ${dataParam[i].currency}. StockExchange: ${dataParam[i].stockExchange}.</small>`;
+        newItem.innerHTML = `<img class="img-icon px-2" src="${arrayOfImages[i]}">
+        </img>
+        <a href="/company.html?symbol=${dataParam[i].symbol}"><b>${dataParam[i].name}</b> (${dataParam[i].symbol})</a>
+        <span> ${arrayOfPrice[i]} $ </span>
+        <span class="main-price p-2"> (${arrayOfChanges[i]}%) </span>
+        <small class="text-secondary">- ${dataParam[i].exchangeShortName}</small> <br> 
+        <small class="text-secondary">Currency: ${dataParam[i].currency}. StockExchange: ${dataParam[i].stockExchange}.</small>`;
+    }
+
+    changeColor();
+}
+
+function changeColor() {
+    let mainPrice = document.querySelectorAll(".main-price");
+    for (let el of mainPrice) {
+        if (el < 0) {
+            el.classList.add('text-danger');
+        }
+        else { el.classList.add('text-success') };
     }
 }
 
-async function getDataResult() { // ----------- Get data from API ----
+async function getDataResult() { // ----------- Get searchData from API ----
     let loader = document.getElementById('loaderID');
     stockName = inputSearch.value;
     showSpinner(loader)
     try {
         const res = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${stockName}&limit=10&exchange=NASDAQ`)
         if (res.ok) {
-            let data = await res.json()
-            let arrayOfSymbols = [];
-            for (let i = 0; i < data.length; i++) {
-                dataSymbol = await data[i].symbol;
-                arrayOfSymbols.push(dataSymbol);
-            }
-            console.log(arrayOfSymbols);
-            console.log(data);
-            await getDataResult2(arrayOfSymbols)
+            let searchData = await res.json()
+            await getDataResult2(searchData)
             removeSpiner(loader);
-            showTheList(data); // show all results (from server)    
         } else {
             console.log("Not Successful")
         }
@@ -53,27 +62,32 @@ async function getDataResult() { // ----------- Get data from API ----
         console.log('error!');
     }
 }
-// getDataResult2()
-async function getDataResult2(arrayOfSymbols) {
-    // showSpinner(companyLoader);
-    // await getDataResult();
-    // stockName = inputSearch.value;
+async function getDataResult2(searchData) {
+    arrayOfImages = [];
+    arrayOfChanges = [];
+    arrayOfSymbols = [];
+    arrayOfPrice = [];
+
     try {
-        for (let i = 0; i < arrayOfSymbols.length; i++) {
-            const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/profile/${arrayOfSymbols[i]}`)
-            // console.log(response)
-            let data = await response.json();
-            let dataImage = await data[0].image;
-            arrayOfImages.push(dataImage);
-            // console.log(data[0].image);
+        for (let i = 0; i < searchData.length; i++) {
+            let dataSymbol = await searchData[i].symbol;
+            arrayOfSymbols.push(dataSymbol);
         }
-        console.log(arrayOfImages[1]);
-        // console.log(data.image);
+
+        for (let i = 0; i < searchData.length; i++) {
+            const response = await fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/profile/${arrayOfSymbols[i]}`)
+            let companyData = await response.json();
+            let dataImage = await companyData[0].image;
+            arrayOfImages.push(dataImage);
+            let priceChanges = await companyData[0].changes;
+            arrayOfChanges.push(priceChanges);
+            let price = await companyData[0].price;
+            arrayOfPrice.push(price);
+        }
+        showTheList(searchData, arrayOfImages, arrayOfChanges, arrayOfPrice); // show all results (from server)
     } catch (error) { console.log('error!'); }
 }
-// getDataResult()
 btnSearch.addEventListener('click', function () {
-    // resultList.innerHTML = ""; //clear the list on the page
     getDataResult()
 
 })
